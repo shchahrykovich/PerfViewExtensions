021e0dbaf32e7fb6ea016c350df98f40c8158c3e
Sergey Shchegrikovich <shchegrikovich@gmail.com>
2015-02-20 21:28:56 +0300

    Added missing files

---
+++ Reporting.Tests/Fakes/FakeExporter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Reporting.Export;
+using Reporting.Implementations.Entities;
+
+namespace Reporting.Tests.Fakes
+{
+    internal class FakeExporter: IExporter
+    {
+        public int CallCounter { get; private set; }
+
+        public List<Diff> Diffs { get; private set; }
+
+        public void Export(string outputFileName, List<Diff> diffs)
+        {
+            CallCounter++;
+
+            Diffs = diffs;
+        }
+    }
+}
