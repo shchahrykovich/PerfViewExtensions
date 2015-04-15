786359b1c86a9446526c299872c00c46515d356e
Sergey Shchegrikovich <shchegrikovich@gmail.com>
2015-04-15 23:24:31 +0300

    Extracted custom sheets

---
+++ Reporting/Viewers/Xlsx/BaseSheet.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Reporting.Implementations;
+
+namespace Reporting.Viewers.Xlsx
+{
+    internal abstract class BaseSheet
+    {
+        protected SheetData SheetData { get; private set; }
+        protected const int OneBasedArray = 1;
+        protected const int HeaderRow = 1;
+
+        internal abstract void AddData(Statistics stat);
+
+        internal abstract void Create(WorkbookPart workBookPart, Sheets sheets);        
+
+        protected void Create(WorkbookPart workBookPart, Sheets sheets, String sheetName)
+        {
+            WorksheetPart part = CreateSheet(workBookPart, sheets, sheetName);
+            SheetData = CreateSheetData(part);
+        }
+
+        protected static IEnumerable<OpenXmlElement> AddHeader(params String[] columnNames)
+        {
+            Row header = new Row
+            {
+                RowIndex = (UInt32)OneBasedArray
+            };
+
+            for (int i = 0; i < columnNames.Length; i++)
+            {
+                Cell column = CreateCell(1, i + OneBasedArray, columnNames[i]);
+                header.Append(column);
+            }
+
+            return new[] { header };
+        }
+
+        protected static void AddRow(List<Row> rows, string name, long value)
+        {
+            int rowIndex = rows.Count + OneBasedArray;
+
+            Row row = new Row
+            {
+                RowIndex = (UInt32)rowIndex
+            };
+
+            row.Append(CreateCell(rowIndex, 1, name));
+            row.Append(CreateCell(rowIndex, 2, value));
+
+            rows.Add(row);
+        }
+
+        protected static void AddRow(List<Row> rows, string name, double value)
+        {
+            int rowIndex = rows.Count + OneBasedArray;
+
+            Row row = new Row
+            {
+                RowIndex = (UInt32)rowIndex
+            };
+
+            row.Append(CreateCell(rowIndex, 1, name));
+            row.Append(CreateCell(rowIndex, 2, value));
+
+            rows.Add(row);
+        }
+
+        protected static Cell CreateCell(int rowIndex, int columnIndex, String value)
+        {
+            Cell cell = new Cell
+            {
+                DataType = CellValues.InlineString,
+                CellReference = GetColumnName(columnIndex) + rowIndex
+            };
+
+            InlineString inlineString = new InlineString();
+            inlineString.AppendChild(new Text
+            {
+                Text = value
+            });
+
+            cell.AppendChild(inlineString);
+
+            return cell;
+        }
+
+        protected static Cell CreateCell(int rowIndex, int columnIndex, double value)
+        {
+            Cell cell = new Cell
+            {
+                DataType = CellValues.Number,
+                CellReference = GetColumnName(columnIndex) + rowIndex,
+                CellValue = new CellValue(value.ToString())
+            };
+
+            return cell;
+        }
+
+        protected static Cell CreateCell(int rowIndex, int columnIndex, int value)
+        {
+            Cell cell = new Cell
+            {
+                DataType = CellValues.Number,
+                CellReference = GetColumnName(columnIndex) + rowIndex,
+                CellValue = new CellValue(value.ToString())
+            };
+
+            return cell;
+        }
+
+        private static string GetColumnName(int columnIndex)
+        {
+            int dividend = columnIndex;
+            string columnName = String.Empty;
+
+            while (dividend > 0)
+            {
+                int modifier = (dividend - 1) % 26;
+                columnName =
+                    Convert.ToChar(65 + modifier).ToString() + columnName;
+                dividend = (int)((dividend - modifier) / 26);
+            }
+
+            return columnName;
+        }
+
+        private static SheetData CreateSheetData(WorksheetPart part)
+        {
+            SheetData sheetData = new SheetData();
+
+            Worksheet worksheet = new Worksheet();
+            worksheet.Append(sheetData);
+            part.Worksheet = worksheet;
+
+            return sheetData;
+        }
+
+        private static WorksheetPart CreateSheet(WorkbookPart workbookPart, Sheets sheets, string sheetName)
+        {
+            uint sheetId = (uint) sheets.ChildElements.Count + 1;
+            String sheetIdName = "rId" + sheetId;
+
+            Sheet sheet = new Sheet()
+            {
+                Name = sheetName,
+                SheetId = (UInt32Value) sheetId,
+                Id = sheetIdName
+            };
+            sheets.Append(sheet);
+
+            return workbookPart.AddNewPart<WorksheetPart>(sheetIdName);
+        }
+    }
+}
---
+++ Reporting/Viewers/Xlsx/RawDataSheet.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Reporting.Implementations;
+
+namespace Reporting.Viewers.Xlsx
+{
+    internal class RawDataSheet: BaseSheet
+    {
+        internal override void Create(WorkbookPart workBookPart, Sheets sheets)
+        {
+            Create(workBookPart, sheets, "Raw data");
+        }
+
+        internal override void AddData(Statistics stat)
+        {
+            IEnumerable<OpenXmlElement> header = AddHeader("TimeStamp", "Type");
+            SheetData.Append(header);
+
+            IEnumerable<OpenXmlElement> rows = AddPoints(stat);
+            SheetData.Append(rows);
+        }
+
+        private static IEnumerable<OpenXmlElement> AddPoints(Statistics stat)
+        {
+            List<Row> rows = new List<Row>();
+
+            for (int i = 0; i < stat.Points.Length; i++)
+            {
+                Point point = stat.Points[i];
+                
+                int rowIndex = i + HeaderRow + OneBasedArray;
+                Row row = new Row
+                {
+                    RowIndex = (UInt32)rowIndex
+                };
+
+                row.Append(CreateCell(rowIndex, 1, point.TimeStamp));
+                row.Append(CreateCell(rowIndex, 2, point.Type.ToString()));
+
+                rows.Add(row);
+            }
+
+            return rows;
+        }
+
+        //if (i == 0)
+        //{
+        //    row.Append(new Cell
+        //    {
+        //        DataType = CellValues.String,
+        //        CellReference = GetColumnName(4) + rowIndex,
+        //        CellValue = new CellValue("Count")
+        //    });
+
+        //    row.Append(new Cell
+        //    {
+        //        DataType = CellValues.Number,
+        //        CellReference = GetColumnName(5) + rowIndex,
+        //        CellValue = new CellValue(stat.Count.ToString())
+        //    });
+        //}
+
+        //if (i == 1)
+        //{
+        //    row.Append(new Cell
+        //    {
+        //        DataType = CellValues.String,
+        //        CellReference = GetColumnName(4) + rowIndex,
+        //        CellValue = new CellValue("Average")
+        //    });
+
+        //    var average = "=AVERAGE(B2:B" + (stat.Count + 1) + ")";
+        //    row.Append(new Cell
+        //    {
+        //        DataType = CellValues.Number,
+        //        CellReference = GetColumnName(5) + rowIndex,
+        //        CellFormula = new CellFormula(average)
+        //    });
+        //}
+
+        //if (i == 2)
+        //{
+        //    row.Append(new Cell
+        //    {
+        //        DataType = CellValues.String,
+        //        CellReference = GetColumnName(4) + rowIndex,
+        //        CellValue = new CellValue("Median")
+        //    });
+
+        //    var median = "=MEDIAN(B2:B" + (stat.Count + 1) + ")";
+        //    row.Append(new Cell
+        //    {
+        //        DataType = CellValues.Number,
+        //        CellReference = GetColumnName(5) + rowIndex,
+        //        CellFormula = new CellFormula(median)
+        //    });
+        //}
+
+    }
+}
